7470aeb053f1038cfce775d2220c053bfee3b550
Marcus Vinicius Bernardino Rangel <marcus.vbrangel@gmail.com>
2023-12-11 05:24:30 +0000

    feat: create a header semantic page

--- webapp/controller/MySemanticPage.controller.js
+++ webapp/controller/MySemanticPage.controller.js
@@ -1,9 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
     function (Controller) {
         "use strict";
 
