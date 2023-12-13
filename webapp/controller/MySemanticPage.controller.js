727554480fe1bc83f0d8f61c512ffdc92ded962f
Marcus Vinicius Bernardino Rangel <marcus.vbrangel@gmail.com>
2023-12-13 01:30:49 +0000

    feat: create a footer button new

--- webapp/controller/MySemanticPage.controller.js
+++ webapp/controller/MySemanticPage.controller.js
@@ -1,12 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("com.mysap.mysapsemanticpage.controller.MySemanticPage", {
+
             onInit: function () {
 
+            },
+
+            onHandlerNew: function() {
+
+                MessageToast.show("It will create a new record", {
+                    duration: 3000,                  // default
+                    width: "15em",                   // default
+                    my: "center bottom",             // default
+                    at: "center bottom",             // default
+                    of: window,                      // default
+                    offset: "0 0",                   // default
+                    collision: "fit fit",            // default
+                    onClose: null,                   // default
+                    autoClose: true,                 // default
+                    animationTimingFunction: "ease", // default
+                    animationDuration: 1000,         // default
+                    closeOnBrowserNavigation: true   // default
+                });
+
             }
+
+
         });
+
     });
